45bbe1571b93c2520d6b373e2e2227f2643572f7
Sonu Shivcharan <sonushivcharan@outlook.com>
2025-01-24 11:29:37 +0000

    add now post found

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Container, PostCard } from "../components";
+import { Button, Container, PostCard } from "../components";
 import appwriteDBService from "../appwrite/db.config";
 import { useDispatch, useSelector } from "react-redux";
 import { setAllPosts } from "../store/postSlice";
+import { Link } from "react-router-dom";
+
 function Home() {
 const posts = useSelector((state)=>state.posts.allPosts);
  console.log(posts);
@@ -16,17 +18,24 @@ const posts = useSelector((state)=>state.posts.allPosts);
       })
     }
   }, []);
-  return (
-    <div className="bg-white dark:bg-slate-900 min-h-screen py-[40px]">
-      <Container>
-        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 items-center">
-        {posts.map((post) => (
-            <PostCard key={post.$id} {...post} />
-        ))}
-        </div>
-      </Container>
+  if(posts.length<=0){
+    return <div className="bg-white dark:bg-slate-900 min-h-screen py-[40px] flex flex-col items-center justify-center gap-5">
+      <h1 className="text-2xl text-gray-200">No posts available. Why not create one?</h1> 
+      <Link to={"/add-post"}><Button>Create Now</Button></Link>
     </div>
-  );
+  }else{
+    return (
+      <div className="bg-white dark:bg-slate-900 min-h-screen py-[40px]">
+        <Container>
+          <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 items-center">
+          {posts.map((post) => (
+              <PostCard key={post.$id} {...post} />
+          ))}
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Home;
