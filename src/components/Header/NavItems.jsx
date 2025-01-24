85cce73b4b07ead1eb098afd623e26f2086a7695
Sonu Shivcharan <sonushivcharan@outlook.com>
2025-01-24 11:29:09 +0000

    rename navlink All Posts to My Posts

--- src/components/Header/NavItems.jsx
+++ src/components/Header/NavItems.jsx
@@ -24,7 +24,7 @@ function NavItems(props) {
       active: !authStatus,
     },
     {
-      name: "All Posts",
+      name: "My Posts",
       slug: "/all-posts",
       active: authStatus,
     },
